d9ef91d64409519d0b80d6633d14236ec3cc08ae
trujilml <mickeytrj@gmail.com>
2021-10-14 23:50:37 -0500

    converted trash into a droppable function

--- assets/js/script.js
+++ assets/js/script.js
@@ -227,6 +227,21 @@ $(".card .list-group").sortable({
   }
 });
 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui){
+    console.log("over");
+  },
+  out: function(event, ui){
+    console.log("out");
+  }
+});
+
 // load tasks for the first time
 loadTasks();
 
